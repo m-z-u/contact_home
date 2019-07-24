3694dbc6ed97385837a68cdd227364d0fcf3d169
NAKANO MIZUKI <nakanomizuki@NAKANOnoMacBook-Air.local>
2019-07-24 11:42:58 +0800

    入力画面とチェック画面の作成

---
+++ check.php
@@ -0,0 +1,21 @@
+<?php
+    $nickname = $_POST['nickname'];
+    echo $nickname;
+    $email = $_POST['email'];
+    echo $email;
+
+    $content = $_POST['content'];
+    echo $content;
+    
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>入力内容確認</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>入力内容確認</h1>
+</body>
+</html>
