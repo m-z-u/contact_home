0e57a632fdf7d251491aaaa497794935f53e7427
NAKANO MIZUKI <nakanomizuki@NAKANOnoMacBook-Air.local>
2019-07-25 10:56:59 +0800

    お問い合わせページ作成

--- check.php
+++ check.php
@@ -1,13 +1,33 @@
 <?php
+
+//getできた場合は、　index.htmlに戻す
+if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+    header('Location: index.html');
+    exit;
+}
+
+
     $nickname = $_POST['nickname'];
-    echo $nickname;
     $email = $_POST['email'];
-    echo $email;
-
     $content = $_POST['content'];
-    echo $content;
-    
 
+    if ($nickname == '') {
+        $nickname_result = 'ニックネームが入力されていません。';
+    } else {
+        $nickname_result = 'ようこそ、' . $nickname .'様';
+    }
+    
+    if ($email == '') {
+        $email_result = 'メールアドレスが入力されていません。';
+    } else {
+        $email_result = 'メールアドレス：' . $email;
+    }
+
+    if ($content == '') {
+        $content_result =  'お問い合わせ内容が入力されていません。';
+    } else {
+        $content_result = 'お問い合わせ内容：' . $content;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -16,6 +36,20 @@
     <meta charset="utf-8">
 </head>
 <body>
-    <h1>入力内容確認</h1>
+<h1>入力内容確認</h1>
+    <p><?php echo $nickname_result; ?></p>
+    <p><?php echo $email_result; ?></p>
+    <p><?php echo $content_result; ?></p>
+    <button type="button" onclick="history.back()">戻る</button>
+    <!-- thanks.php にポスト送信するためのボタン-->
+    <form method="POST" action="thanks.php">
+        <input type="hidden" name="nickname" value="<?php echo $nickname; ?>">
+        <input type="hidden" name="email" value="<?php echo $email; ?>">
+        <input type="hidden" name="content" value="<?php echo $content; ?>">
+        <input type="button" value="戻る" onclick="history.back()">
+        <?php if ($nickname != '' && $email != '' && $content != ''): ?>
+            <input type="submit" value="OK">
+        <?php endif; ?>
+    </form>
 </body>
 </html>
